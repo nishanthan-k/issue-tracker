a93b899d849a2ce4ba8df7bd2a293c44952ab5a6
nishanthan-k <krn.nishanthan@gmail.com>
2024-09-03 14:52:16 +0530

    feat: add a markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,13 @@
-import { Button, TextArea, TextField } from '@radix-ui/themes';
+"use client";
+import { Button, TextField } from '@radix-ui/themes';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssue = () => {
   return (
     <div className='max-w-xl space-y-3'>
       <TextField.Root placeholder='Title'/>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
       <Button>Submit</Button>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import '@radix-ui/themes/styles.css';
-import { Theme, ThemePanel } from '@radix-ui/themes';
+import { Theme } from '@radix-ui/themes';
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
