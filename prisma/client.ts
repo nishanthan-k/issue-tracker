8b1ae94d96e96ca600d7b2d012511cea0579ed69
nishanthan-k <krn.nishanthan@gmail.com>
2024-09-13 10:09:46 +0530

    fix: prisma globathis to global

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,15 +1,12 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
+declare global {
+  // Ensure the global object has the prisma property
+  var prisma: PrismaClient | undefined;
 }
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+const prisma = global.prisma || new PrismaClient();
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export default prisma;
